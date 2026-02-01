e23b7ae8dc69ab56221989a4751c562cd1ed7e7d
Phuc Le <phuc05112005@gmail.com>
2026-02-01 13:45:37 +0700

    Update 13:45

---
+++ clothing_store/products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    price = models.IntegerField()
+    description = models.TextField()
+    image = models.ImageField(upload_to="products/")
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
