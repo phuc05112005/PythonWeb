de44ecba01f3a1ca5ecf9778eb0029fb0a74514c
CuBin22207 <phitruong22072005@gmail.com>
2026-05-06 20:52:13 +0700

    push file backup vs word

--- clothing_store/settings.py
+++ clothing_store/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'shopthoitrang',
         'USER': 'postgres',
-        'PASSWORD': '123',
+        'PASSWORD': '22072005',
         'HOST': '127.0.0.1',        
         'PORT': '5432',          
     }
